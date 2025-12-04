c4710c62a1c1f16737f55a56b5f10a88d762c77a
obk369369-spec <obk369369@gmail.com>
2025-12-04 16:02:39 +0900

    Delete index.ts

--- index.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from "https://deno.land/std/http/server.ts";
-
-const server = serve({ port: 8080 });
-console.log("Server running on http://localhost:8080");
-
-for await (const req of server) {
-  req.respond({ body: await Deno.readTextFile("./index.html") });
-}
